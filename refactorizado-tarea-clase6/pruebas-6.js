70f308d6bda71ab722f8ab06cb48719226cce2e2
Rafael <95598903+rafael-romero@users.noreply.github.com>
2022-01-24 20:56:04 -0300

    agrega pruebas para validar edad familiares

--- refactorizado-tarea-clase6/pruebas-6.js
+++ refactorizado-tarea-clase6/pruebas-6.js
@@ -1,17 +1,12 @@
 function probarValidarCantidadFamiliares(){
     console.assert(
-        validarCantidadFamiliares("") === "Debe ingresar al menos un familiar",
+        validarCantidadFamiliares("0") === "Debe ingresar al menos un familiar",
         "ValidarCantidadFamiliares no valido que la cantidad sea mayor a cero"  
     );
 
     console.assert(
-        validarCantidadFamiliares("1.23") === "Solo puede ingresar numeros enteros",
+        validarCantidadFamiliares("1.23") === "Solo puede ingresar numeros enteros positivos",
         "ValidarCantidadFamiliares no valido que el numero ingresado sea entero"
-    );
-
-    console.assert(
-        validarCantidadFamiliares("101") === "Es asombroso que tenga tantos familiares",
-        "ValidarCantidadFamiliares no valido que el numero ingresado sea coherente"
     );
 
     console.assert(
@@ -21,5 +16,23 @@ function probarValidarCantidadFamiliares(){
 
 }
 
+function probarValidarEdadFamiliares(){
+    console.assert(
+        validarEdadFamiliares("0") === "Debe ingresar la edad de su familiar",
+        "validarEdadFamiliares no valido que la edad sea mayor a cero"
+    );
+    
+    console.assert(
+        validarEdadFamiliares("3.85") === "Solo puede ingresar numeros enteros positivos",
+        "ValidarEdadFamiliares no valido que el numero ingresado sea entero"
+    );
+
+    console.assert(
+        validarEdadFamiliares("43") === "",
+        "validarEdadFamiliares fallo la validacion con un numero valido"
+    );
+}
+
 
 probarValidarCantidadFamiliares();
+probarValidarEdadFamiliares();

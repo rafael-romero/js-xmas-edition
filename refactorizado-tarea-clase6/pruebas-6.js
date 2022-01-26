716f7911493fdd28d85ac10638f203dbd9012fc4
Rafael <95598903+rafael-romero@users.noreply.github.com>
2022-01-26 12:42:31 -0300

    corrige error de tipeo

--- refactorizado-tarea-clase6/pruebas-6.js
+++ refactorizado-tarea-clase6/pruebas-6.js
@@ -1,16 +1,16 @@
 function probarValidarCantidadFamiliares(){
     console.assert(
-        validarCantidadFamiliares("0") === "Debe ingresar al menos un familiar",
+        validarCantidadFamiliares(0) === "Debe ingresar al menos un familiar",
         "ValidarCantidadFamiliares no valido que la cantidad sea mayor a cero"  
     );
 
     console.assert(
-        validarCantidadFamiliares("1.23") === "Solo puede ingresar numeros enteros positivos",
+        validarCantidadFamiliares(1.23) === "Solo puede ingresar numeros enteros positivos",
         "ValidarCantidadFamiliares no valido que el numero ingresado sea entero"
     );
 
     console.assert(
-        validarCantidadFamiliares("7") === "",
+        validarCantidadFamiliares(7) === "",
         "ValidarCantidadFamiliares fallo la validacion con un numero valido"
     );
 
@@ -18,17 +18,17 @@ function probarValidarCantidadFamiliares(){
 
 function probarValidarEdadFamiliares(){
     console.assert(
-        validarEdadFamiliares("0") === "Debe ingresar la edad de su familiar",
+        validarEdadFamiliares(0) === "Debe ingresar la edad de su familiar",
         "validarEdadFamiliares no valido que la edad sea mayor a cero"
     );
     
     console.assert(
-        validarEdadFamiliares("3.85") === "Solo puede ingresar numeros enteros positivos",
+        validarEdadFamiliares(3.85) === "Solo puede ingresar numeros enteros positivos",
         "ValidarEdadFamiliares no valido que el numero ingresado sea entero"
     );
 
     console.assert(
-        validarEdadFamiliares("43") === "",
+        validarEdadFamiliares(43) === "",
         "validarEdadFamiliares fallo la validacion con un numero valido"
     );
 }

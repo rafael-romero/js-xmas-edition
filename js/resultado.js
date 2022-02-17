2c09288e7ffd222177c9141f6e94bba6d8bf81f7
Rafael <95598903+rafael-romero@users.noreply.github.com>
2022-02-17 13:11:25 -0300

    agrega funcionalidad de mostrar regalo
    completa el ejercicio de forma que se entrega visualmente un regalo

--- js/resultado.js
+++ js/resultado.js
@@ -20,3 +20,15 @@ function obtenerRegaloAleatorio(){
     const regaloSeleccionado = JSON.parse(localStorage.getItem(`usuario${[numeroDeRegaloAleatorio]}`)).descripcionRegalo;
     document.querySelector(".santa-gift-text").textContent = regaloSeleccionado;
 }   
+
+const listaDeImagenesURL = {
+    1: 'img/gifts/goldGift.jpg',
+    2: 'img/gifts/groupGift.jpeg',
+    3: 'img/gifts/redGift.jpg'
+}
+
+function obtenerImagenRegalo(){
+    const NUMERO_REGALOS_EN_CARPETA = 3;
+    const numeroRegaloElegido = generaNumeroAleatorio(NUMERO_REGALOS_EN_CARPETA);
+    document.querySelector(".santa-gift").src = listaDeImagenesURL[numeroRegaloElegido];
+}
